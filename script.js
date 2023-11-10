bfbff73c8318e7be880dfaf935a3a5d32c8b7730
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2023-11-10 11:56:41 +0100

    Add form validation script and styles

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
-let psw_check = document.querySelector(".psw_check")
+let check = function () {
+    let password = document.getElementById("password");
+    let confirm_password = document.getElementById("confirm_password");
+    let psw_check = document.getElementById("psw_check");
+    if (password.value ===
+        confirm_password.value) {
+        confirm_password.setCustomValidity('');
+        psw_check.textContent = "";
+    } else {
+        confirm_password.setCustomValidity("Passwords do not match");
+        confirm_password.style.borderColor = "red";
+        psw_check.style.color = "red";
+        psw_check.textContent = "*Passwords do not match";
+    }
+};
 
-psw_check.textContent = "*Passwords do not match"
+let input = document.querySelectorAll(".input");
+input.forEach(function (el) {
+    el.addEventListener('keyup', input_check);
+})
+
+function input_check(event) {
+    if (event.target.validity.valid) {
+        event.target.style.borderColor = "rgb(0, 191, 255)";
+    } else {
+        event.target.style.borderColor = "red";
+    }
+};
